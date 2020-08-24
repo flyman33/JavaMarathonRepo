4f351e0a5d149a3a11e8ccc3e3162fd7cf631871
Vladimir Kuznetsov <flyman335@gmail.com>
2020-08-25 00:44:41 +0800

    Complete day1 tasks

---
+++ src/day1/Task4.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        int age = 26;
+
+        while(age < 100) {
+            System.out.println("Мой возраст " + age);
+            age += 5;
+        }
+    }
+
+}
---
+++ src/day1/Task5.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+        int age = 26;
+
+        for(int i = age; i < 100; i += 5) {
+            System.out.println("Мой возраст " + i);
+        }
+    }
+}
