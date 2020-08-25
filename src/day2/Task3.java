ae57534738674be8ed0f7a9374857f4da3e575c3
Vladimir Kuznetsov <flyman335@gmail.com>
2020-08-25 21:35:09 +0800

    Complete day2 tasks

---
+++ src/day2/Task3.java
@@ -0,0 +1,25 @@
+package day2;
+
+import java.util.Scanner;
+
+//Реализовать программу №2, используя цикл while.
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите числа a и b...");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        scanner.close();
+
+        while(a < b) {
+            if(a % 5 == 0 && a % 10 != 0)
+                System.out.print(a + " ");
+            a++;
+        }
+    }
+
+}
